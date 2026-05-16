2ded83fd65838945ecb5a6cd147ee2b1c87240a7
kalpesh.chavhan <kalpeshchavhan1234@gmail.com>
2026-05-16 05:56:54 +0530

    Update files

--- src/test/java/com/qa/tests/SauceDemoTest.java
+++ src/test/java/com/qa/tests/SauceDemoTest.java
@@ -1,23 +1,23 @@
 package com.qa.tests;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 
 public class SauceDemoTest {
 
 	WebDriver driver;
-
-	// Change browser here: chrome or edge
-	
 	String browser = "chrome";
 
 	@BeforeMethod
@@ -25,34 +25,39 @@ public class SauceDemoTest {
 
 		if (browser.equalsIgnoreCase("chrome")) {
 
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			Map<String, Object> prefs = new HashMap<>();
+			prefs.put("credentials_enable_service", false);
+			prefs.put("profile.password_manager_enabled", false);
+			prefs.put("profile.password_manager_leak_detection", false);
+
+			options.setExperimentalOption("prefs", prefs);
+			options.addArguments("--disable-notifications");
+			driver = new ChromeDriver(options);
 
 		} else if (browser.equalsIgnoreCase("edge")) {
-
 			driver = new EdgeDriver();
 		}
 
 		driver.manage().window().maximize();
-
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://www.saucedemo.com/");
 	}
 
+	public void login() {
+
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+	}
+
 	@Test(priority = 1)
 	public void verifyValidLogin() {
 
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		login();
 
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-
-		driver.findElement(By.id("login-button")).click();
-
-		String expectedUrl = "https://www.saucedemo.com/inventory.html";
-
-		String actualUrl = driver.getCurrentUrl();
-
-		Assert.assertEquals(actualUrl, expectedUrl);
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
 
 		System.out.println("Login Test Passed");
 	}
@@ -60,19 +65,13 @@ public class SauceDemoTest {
 	@Test(priority = 2)
 	public void verifyAddProductToCart() {
 
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-
-		driver.findElement(By.id("login-button")).click();
+		login();
 
 		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
 
 		WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
 
-		String actualCount = cartBadge.getText();
-
-		Assert.assertEquals(actualCount, "1");
+		Assert.assertEquals(cartBadge.getText(), "1");
 
 		System.out.println("Add To Cart Test Passed");
 	}
@@ -80,26 +79,17 @@ public class SauceDemoTest {
 	@Test(priority = 3)
 	public void verifySuccessfulCheckout() {
 
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-
-		driver.findElement(By.id("login-button")).click();
+		login();
 
 		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-
 		driver.findElement(By.className("shopping_cart_link")).click();
-
 		driver.findElement(By.id("checkout")).click();
 
 		driver.findElement(By.id("first-name")).sendKeys("Kalpesh");
-
 		driver.findElement(By.id("last-name")).sendKeys("Chavan");
-
 		driver.findElement(By.id("postal-code")).sendKeys("410203");
 
 		driver.findElement(By.id("continue")).click();
-
 		driver.findElement(By.id("finish")).click();
 
 		String successMessage = driver.findElement(By.className("complete-header")).getText();
@@ -111,7 +101,6 @@ public class SauceDemoTest {
 
 	@AfterMethod
 	public void closeBrowser() {
-
 		driver.quit();
 	}
 }
